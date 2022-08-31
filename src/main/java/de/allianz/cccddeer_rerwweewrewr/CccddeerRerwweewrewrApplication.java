4d060881ee7aec06bb3438a8277a4f35f9d38715
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-08-31 14:13:52 +0200

    Create maven project

---
+++ src/main/java/de/allianz/cccddeer_rerwweewrewr/CccddeerRerwweewrewrApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.cccddeer_rerwweewrewr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccddeerRerwweewrewrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccddeerRerwweewrewrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/cccddeer_rerwweewrewr/CccddeerRerwweewrewrApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.cccddeer_rerwweewrewr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccddeerRerwweewrewrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
